22c718c1830594bf9fa1e60a81877ceca17d26ec
farid <farid9811@gmail.com>
2023-03-17 17:40:12 -0500

    index y menu deplegable

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktop_menu = document.querySelector('.desktop-menu');
 
 navbar_email.addEventListener('click' , toggleshowDesktopMenu)
 
-function toggleshowDesktopMenu(desktop_menu){
+function toggleshowDesktopMenu(){
     desktop_menu.classList.toggle('inactive');
 }
 
