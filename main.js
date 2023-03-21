c3e0d57ea38e6b15fc805b5613ed5ea9bd9dd86e
farid <farid9811@gmail.com>
2023-03-21 11:09:30 -0500

    Media query para menu desplegable mobile y para menu desktop

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const navbar_email = document.querySelector('.navbar-email');
 const desktop_menu = document.querySelector('.desktop-menu');
+const mobile_menu = document.querySelector('.mobile-menu');
+const menu_hamburgesa = document.querySelector('.menu');
 
 navbar_email.addEventListener('click' , toggleshowDesktopMenu)
 
+menu_hamburgesa.addEventListener('click', toggleshowpMobileMenu)
+
+
+function toggleshowpMobileMenu(){
+
+    mobile_menu.classList.toggle('inactive-mobile');
+    console.log('verga')
+}
+
 function toggleshowDesktopMenu(){
+
     desktop_menu.classList.toggle('inactive');
 }
 
