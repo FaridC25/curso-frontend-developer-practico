24c092bad1597cfcc5fe73e5a7ab55acb1680f98
farid <farid9811@gmail.com>
2023-03-23 12:05:31 -0500

    Se añadieron los articulos de manera dinamica en la clase main y en el style se añadio el estilo de los articulos y en el index se añadieron los containers de los articulos

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const mobile_menu = document.querySelector('.mobile-menu');
 const menu_hamburgesa = document.querySelector('.menu');
 const navbar_shopping_cart = document.querySelector('.navbar-shopping-cart');
 const product_detail = document.querySelector('.product-detail');
+const cards_container = document.querySelector('.cards-container');
 
 navbar_email.addEventListener('click' , toggleshowDesktopMenu)
 
@@ -51,3 +52,88 @@ function toggleshowDesktopMenu(){
     }
 }
 
+
+
+const product_list = [];
+
+product_list.push({
+    name: 'Bike',
+    price: 120,
+    Image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+product_list.push({
+    name: 'TV',
+    price: 600,
+    Image: 'https://i0.wp.com/towncenter.co/wp-content/uploads/2022/07/Televisor-LG-32%E2%80%B3-Smart-TV-HD.webp?fit=800%2C800&ssl=1',
+});
+
+product_list.push({
+    name: 'PC GAMER',
+    price: 1000,
+    Image: 'https://exitocol.vtexassets.com/arquivos/ids/11640443/computador-pc-de-escritorio-torre-gamer-power-group-amd-ryzen-5-3500-ssd-240gb-ram-8gb-led-22-full-hd.jpg?v=637786563587930000',
+});
+
+
+product_list.push({
+    name: 'Sound Equipment ',
+    price: 300,
+    Image: 'https://i.pinimg.com/236x/c5/c4/c2/c5c4c23313b8dcbe69e130437b426c5a--sony-product-design.jpg',
+});
+
+
+product_list.push({
+    name: 'XBOX SERIES X',
+    price: 300,
+    Image: 'https://falabella.scene7.com/is/image/FalabellaCO/9390605_1?wid=800&hei=800&qlt=70',
+});
+
+product_list.push({
+    name: 'PS5',
+    price: 300,
+    Image: 'https://falabella.scene7.com/is/image/FalabellaCO/61897888_1?wid=800&hei=800&qlt=70',
+});
+
+
+function renderizar_productos(array){
+
+for ( product of array) {
+
+    const product_card = document.createElement('div');
+    product_card.classList.add('product-card');
+
+    const product_img = document.createElement('img');
+    product_img.setAttribute('src',product.Image);
+
+
+    const product_info = document.createElement('div');
+    product_info.classList.add('product-info');
+
+    const product_div = document.createElement('div');
+    const product_price = document.createElement('p');
+    product_price.innerText='$'+product.price;
+    
+    const product_name = document.createElement('p');
+    product_name.innerText=product.name;
+
+    product_div.append(product_price,product_name);
+    
+
+    const product_figure = document.createElement('figure');
+    const img_figure = document.createElement('img');
+    img_figure.setAttribute('src','./icons/bt_add_to_cart.svg');
+    product_figure.append(img_figure)
+    
+
+    product_info.append(product_div,product_figure);
+    
+
+    product_card.append(product_img,product_info);
+    
+    
+    cards_container.append(product_card);
+  
+}
+};
+
+renderizar_productos(product_list);
